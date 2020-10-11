0bce45a38b56625356775b97176e02ef8ecdc489
Seth <developer.sds100@gmail.com>
2020-10-11 15:08:03 +0100

    remove unused about class

--- app/src/main/java/io/github/sds100/keymapper/inputmethod/latin/utils/FragmentUtils.java
+++ app/src/main/java/io/github/sds100/keymapper/inputmethod/latin/utils/FragmentUtils.java
@@ -16,7 +16,6 @@
 
 package io.github.sds100.keymapper.inputmethod.latin.utils;
 
-import io.github.sds100.keymapper.inputmethod.latin.about.AboutPreferences;
 import io.github.sds100.keymapper.inputmethod.latin.settings.AdvancedSettingsFragment;
 import io.github.sds100.keymapper.inputmethod.latin.settings.AppearanceSettingsFragment;
 import io.github.sds100.keymapper.inputmethod.latin.settings.CorrectionSettingsFragment;
@@ -37,7 +36,6 @@ import java.util.HashSet;
 public class FragmentUtils {
     private static final HashSet<String> sLatinImeFragments = new HashSet<>();
     static {
-        sLatinImeFragments.add(AboutPreferences.class.getName());
         sLatinImeFragments.add(PreferencesSettingsFragment.class.getName());
         sLatinImeFragments.add(AppearanceSettingsFragment.class.getName());
         sLatinImeFragments.add(ThemeSettingsFragment.class.getName());
